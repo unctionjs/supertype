3b50bdc56bf74473ee0411d24fb96172fda78d09
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:31 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-import type from "@unction/type"
-
-import type {StringType} from "typess"
-
-export default function supertype (value: mixed): StringType {
-  return type(Reflect.getPrototypeOf(value.constructor.prototype))
+import type from "@unction/type";
+export default function supertype(value) {
+  return type(Reflect.getPrototypeOf(value.constructor.prototype));
 }
