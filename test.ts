317a5b8eb0353365f60ccd5ca728049dbdb27817
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-06 14:56:52 -0700

    The transformer didnt fix this

--- test.ts
+++ test.ts
@@ -1,11 +1,11 @@
 
 import supertype from "./index";
 
-test(() => {
+test("works", () => {
   expect(supertype("a")).toEqual("Object");
 });
 
-test(() => {
+test("works", () => {
   class Assistant extends class Job {} {}
 
   expect(supertype(new Assistant())).toEqual("Job");
